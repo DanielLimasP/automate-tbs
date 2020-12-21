95c9580632d4ebaf7f8edffcbbddb3e8007b15eb
Daniel Limas <amedlimas97@gmail.com>
2020-12-21 11:23:18 -0700

    Finished img chapter

--- add-logo.py
+++ add-logo.py
@@ -1,34 +1,37 @@
-   #! python3
-   # add-logo.py - Resizes all images in current working directory to fit
-   # in a 300x300 square, and adds catlogo.png to the lower-right corner.
+#! python3
+# add-logo.py - Resizes all images in current working directory to fit
+# in a 300x300 square, and adds catlogo.png to the lower-right corner.
 
-   import os
-   from pathlib import Path
-   from PIL import Image
+import os
+from pathlib import Path
+from PIL import Image
 
-   SQUARE_FIT_SIZE = 300
-   LOGO_FILENAME = Path.cwd / 'img' / 'logo.jpg'
+SQUARE_FIT_SIZE = 300
+LOGO_FILENAME = Path.cwd / 'img' / 'logo.jpg'
 
-   logo_img = Image.open(LOGO_FILENAME)
-   logo_w, logo_h = logo_img.size
+logo_img = Image.open(LOGO_FILENAME)
+logo_w, logo_h = logo_img.size
 
-   # Loop through dir to find images
-   for filename in os.listdir(Path.cwd() / 'img'):
-       if not (filename.endswith('.png') or filename.endswith('.jpg') or filename == LOGO_FILENAME):
-           continue
+# Loop through dir to find images
+for filename in os.listdir(Path.cwd() / 'img'):
+    if not (filename.endswith('.png') or filename.endswith('.jpg') or filename == LOGO_FILENAME):
+        continue
 
-        img_file = Image.open(filename)
-        w, h = img_file.size
+    img_file = Image.open(filename)
+    w, h = img_file.size
 
-        if w > SQUARE_FIT_SIZE and h > SQUARE_FIT_SIZE:
-            if w > h:
-                h = int((SQUARE_FIT_SIZE / w) * h)
-                w = SQUARE_FIT_SIZE
-            else:
-                w = int((SQUARE_FIT_SIZE / w) * h)
-        
+    if w > SQUARE_FIT_SIZE and h > SQUARE_FIT_SIZE:
+        if w > h:
+            h = int((SQUARE_FIT_SIZE / w) * h)
+            w = SQUARE_FIT_SIZE
+        else:
+            w = int((SQUARE_FIT_SIZE / w) * h)
+
         print('Resizing {}'.format(filename))
         img_file = img_file.resize((w, h))
 
         print('Adding logo to {}'.format(filename))
-        img_file.paste()
+        img_file.paste(logo_img, (w - logo_w, h - logo_h), logo_img)
+
+        # Save the changes
+        img_file.paste(os.path.join(Path.cwd() / 'with_logo' / filename))
--- image.py
+++ image.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 catIm = Image.open(cwd / 'zophie.png')
 catIm.size
@@ -30,3 +30,16 @@ catIm.rotate(180).save('rotated180.png')
 catIm.rotate(270).save('rotated270.png')
 catIm.transpose(Image.FLIP_LEFT_RIGHT).save('horizontal_flip.png')
 catIm.transpose(Image.FLIP_TOP_BOTTOM).save('vertical_flip.png')
+
+# Drawing
+im = Image.new('RGBA', (200, 200), 'white')
+draw = ImageDraw.Draw(im)
+draw.line([(0, 0), (199, 0), (199, 199), (0, 199), (0, 0)], fill='black')
+draw.rectangle((20, 30, 60, 60), fill='blue')
+draw.ellipse((120, 30, 160, 60), fill='red')
+draw.polygon(((57, 87), (79, 62), (94, 85), (120, 90), (103, 113)), fill='brown')
+
+for i in range(100, 200, 10):
+    draw.line([(i, 0), (200, i - 100)], fill='green')
+
+im.save('drawing.png')
